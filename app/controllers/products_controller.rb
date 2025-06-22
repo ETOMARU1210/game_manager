053160704c4e3a99bcdbe9eac22ada07121b8fe2
HoriuchiInori <inori0218adt@gmail.com>
2025-06-22 19:22:22 +0900

    htmlにrails用のformにし、コントローラーを適切な形に変更

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,7 @@
 class ProductsController < ApplicationController
 
     def show
+        @task = Task.new
+        @post = Post.new
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   root "home#index"
   resources :products
+  resources :tasks, only: [:create, :destroy]
+  resources :posts, only: [:create, :destroy]
 end
